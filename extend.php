95bab9a0bc92a0d704e0c7be0fb0f99afb8895c5
forumaker <arsenyuxw@gmail.com>
2026-06-02 12:49:43 +0300

    feat: pills layout

--- extend.php
+++ extend.php
@@ -18,7 +18,11 @@ return [
     (new Extend\Settings())
         ->default('forumaker-bento.columns_desktop', '4')
         ->default('forumaker-bento.plain_create_button', '1')
+        ->default('forumaker-bento.layout', 'tiles')
+        ->default('forumaker-bento.pill_shape', 'capsule')
 
         ->serializeToForum('forumaker-bento.columns_desktop', 'forumaker-bento.columns_desktop', 'intval')
-        ->serializeToForum('forumaker-bento.plain_create_button', 'forumaker-bento.plain_create_button', 'boolval'),
+        ->serializeToForum('forumaker-bento.plain_create_button', 'forumaker-bento.plain_create_button', 'boolval')
+        ->serializeToForum('forumaker-bento.layout', 'forumaker-bento.layout')
+        ->serializeToForum('forumaker-bento.pill_shape', 'forumaker-bento.pill_shape'),
 ];
--- js/admin.ts
+++ js/admin.ts
@@ -1,4 +1,2 @@
 import app from 'flarum/admin/app';
 export { default as extend } from './src/admin/extend';
-
-app.initializers.add('forumaker-bento', () => {});
